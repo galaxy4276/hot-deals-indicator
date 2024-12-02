41517158be96c355e4abdaf5aaa2a958758e2adf
DonghyeokKoh <donghyeokgo@gmail.com>
2024-12-02 15:34:29 +0900

    import AsyncElasticsearch

--- publisher/database.py
+++ publisher/database.py
@@ -1,28 +1,17 @@
-from elasticsearch import Elasticsearch
+from elasticsearch import AsyncElasticsearch
 
-es = Elasticsearch(["http://han-box.co.kr:9200"], basic_auth=("", ""))
+es = AsyncElasticsearch(["http://han-box.co.kr:9200"], basic_auth=("", ""))
 
 async def search_products(product_name, max_price):
     query = {
         "query": {
             "bool": {
                 "must": [
-                    {"match_all": {}},
-                    {
-                        "script": {
-                            "script": {
-                                "source": """
-                                    def price_str = doc['price.keyword'].value;
-                                    if (price_str == null || price_str.empty) return false;
-                                    def price_num = price_str.replace(',', '');
-                                    return Double.parseDouble(price_num) <= params.max_price;
-                                """,
-                                "params": {
-                                    "max_price": float(max_price)
-                                }
-                            }
-                        }
-                    }
+                    # {"match_all": {}}, # for test
+                    {"match": {"name": product_name}},
+
+                    {"range": {"price": {"lte": max_price}}}
+
                 ]
             }
         },
