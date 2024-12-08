9f43f74a066b91df2f2a9971b270086544389cb3
DonghyeokKoh <donghyeokgo@gmail.com>
2024-12-08 20:52:42 +0900

    delete id, pwd

--- publisher/settings.py
+++ publisher/settings.py
@@ -3,8 +3,8 @@ from typing import List
 
 class AppSettings(BaseSettings):
     ELASTICSEARCH_HOST: List[str] = ["http://han-box.co.kr:9200"]
-    ELASTICSEARCH_USERNAME: str = "elastic"
-    ELASTICSEARCH_PASSWORD: str = "rhehdgur12"
+    ELASTICSEARCH_USERNAME: str = ""
+    ELASTICSEARCH_PASSWORD: str = ""
     MONITORING_INTERVAL: int = 10
     MAX_RESULTS_PER_SEARCH: int = 15
     WEBSOCKET_TIMEOUT: int = 30
